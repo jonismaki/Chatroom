305e3a8746b8fcb57c44b15ebe9068c341600494
unknown <joni.smaki@outlook.com>
2022-10-13 20:01:53 +0300

    Cryptor toimii

--- Chatroom/Client.py
+++ Chatroom/Client.py
@@ -10,46 +10,45 @@ from Cryptor import Cryptor
 
 ###################### FUNKTIOT #############################
 
-def receive():
+def receive(nickname, client, cryptor):
     while True:
-        try:
-            message = client.recv(1024)
-            nickTest = message.decode('UTF-8')
-            if nickTest == 'NICK':
-                client.send(nickname.encode('UTF-8'))
-                pass
-            else:
+        message = client.recv(1024)
+        nickTest = message.decode('UTF-8')
+        if nickTest == 'NICK':
+            client.send(nickname.encode('UTF-8'))
+            pass
+        else:
+            try:
+                # FAILS, IF MESSAGE COMES FROM SERVER
+                message = cryptor.decode(message)
+                message_list = message.split()
                 try:
-                    # FAILS, IF MESSAGE COMES FROM SERVER
-                    message = decode(message)
-                    message_list = message.split()
                     if message_list[1] == "~cmd":
-                        cmd(message_list)
+                        cmd(message_list, client, nickname, cryptor)
                     else:
                         print(message)
+                except Exception as e:
+                        print(e)
+                        continue
 
-                except:
-                    # IF MESSAGE COMES FROM THE SERVER
-                    message = message.decode('UTF-8')
-                    print(message)
+            except:
+                # IF MESSAGE COMES FROM THE SERVER
+                message = message.decode('UTF-8')
+                print(message)
 
-        except:
-            print("Receive function failed")
-            client.close()
-            break
 
-def write():
+def write(nickname, client, cryptor):
     while True:
         message_input = input("")
         if len(message_input) == 0:
             pass
         else:
             message = f'{nickname}: {message_input}'
-            message = encode(message)
+            message = cryptor.encode(message)
             client.send(message)
 
 
-def cmd(commands):
+def cmd(commands, client, nickname, cryptor): # SENSUURI TAPAHTUU
 
     del commands[0:2]
     commands_str = " ".join(commands)
@@ -62,7 +61,7 @@ def cmd(commands):
         return
     output = f'{nickname}: {output}'
     try:
-        output_encoded = encode(output)
+        output_encoded = cryptor.encode(output)
         client.send(output_encoded)
     except Exception as e:
         print("Failed to encode")
@@ -75,28 +74,32 @@ def cmd(commands):
 
 ########################### MAIN ####################################
 
-if len(sys.argv) != 4:
-    print("Correct usage: script, IP address, port number, key, nickname")
-    exit(1)
+def main():
 
-host = str(sys.argv[1])
-port = int(sys.argv[2])
-nickname = str(sys.argv[3])
+    if len(sys.argv) != 4:
+        print("Correct usage: script, IP address, port number, key, nickname")
+        exit(1)
 
-key = input("Give key in 16 characters, else the default key will be used: ")
-default_key = "abcdefghijklmnop"
-if len(key) == 0:
-    key = default_key
-key = key.encode('UTF-8')
+    host = str(sys.argv[1])
+    port = int(sys.argv[2])
+    nickname = str(sys.argv[3])
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host, port))
-print("Connected to host")
+    key = input("Give key in 16 characters, else the default key will be used: ")
+    default_key = "abcdefghijklmnop"
+    if len(key) == 0:
+        key = default_key
+    cryptor = Cryptor(key)
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+    print("Connected to host")
 
 
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
+    receive_thread = threading.Thread(target=receive, args=(nickname, client, cryptor))
+    receive_thread.start()
 
-write_thread = threading.Thread(target=write)
-write_thread.start()
+    write_thread = threading.Thread(target=write, args=(nickname, client, cryptor))
+    write_thread.start()
 
+if __name__ == "__main__":
+    main()
--- Chatroom/Cryptor.py
+++ Chatroom/Cryptor.py
@@ -1,3 +1,4 @@
+import hashlib
 import socket
 import subprocess
 import threading
@@ -9,31 +10,36 @@ import Crypto.Random
 
 class Cryptor:
 
-    def encode(message):
+    def __init__(self, init_key):
+        #kysypalikasta juhalta
+        self.key = hashlib.sha256(init_key.encode("utf-8")).digest()
+
+
+    def encode(self, message):
         message = message.encode('UTF-8')
-        message = encypt(message)
+        message = self.encypt(message)
         message = base64.b64encode(message)
         return message
 
-    def decode(message):
+    def decode(self, message):
         message = base64.b64decode(message)
-        message = decrypt(message)
+        message = self.decrypt(message)
         message = message.decode('UTF-8')
         return message
 
-    def encypt(message):
+    def encypt(self, message):
         # FUNCTION ONLY CALLED FROM ENCODE
         iv = Crypto.Random.get_random_bytes(AES.block_size)
-        cipher = AES.new(key, AES.MODE_CBC, iv)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
 
         message = Crypto.Util.Padding.pad(message, AES.block_size)
         message = iv + cipher.encrypt(message)
         return message
 
-    def decrypt(message):
+    def decrypt(self, message):
         # FUNCTION ONLY CALLED FROM DECODE
         iv = message[:AES.block_size]
-        cipher_dec = AES.new(key, AES.MODE_CBC, iv)
+        cipher_dec = AES.new(self.key, AES.MODE_CBC, iv)
         message = cipher_dec.decrypt(message[AES.block_size:])
         message = Crypto.Util.Padding.unpad(message, AES.block_size)
         return message
